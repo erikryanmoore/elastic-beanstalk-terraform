7705b227584afb43d94a87a66945ef9aeacccebb
erikryanmoore <erikmoore02@gmail.com>
2019-02-11 20:02:49 -0800

    Add index.html file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Test EB and Terraform!'));
+app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
